e3c2e354e9fa44c550b20c4b41aef55d61db15cd
Yahor Harai <egor.garai@gmail.com>
2022-08-16 20:22:55 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.min + this.max) / 2)
+        return this.result
     }
 
     lower() {
-
+        this.max = this.result
+        this.guess()
     }
 
     greater() {
-
+        this.min = this.result
+        this.guess()
     }
 }
 
